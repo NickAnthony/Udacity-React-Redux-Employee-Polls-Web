526048ffb53ae3c3797a4cc97ce47f3b49377275
Nick Anthony <nickanthony45@gmail.com>
2023-02-07 12:46:25 -0500

    Update the Question page for more interactability
    - Fixed bug where Question would show authedUser instead of the question author.
    - Answered questions are now formatted as such.  The authed user sees their
      answer and the percentage of answers for each questions!
    - When you vote on a question, it immediately shows you the answer.
    
    This requires two new presentation al components:
    
    - AnsweredOption
    - UserAnswer

---
+++ src/components/AnsweredOption.js
@@ -0,0 +1,25 @@
+/* AnsweredOption is a presentational component that shows
+ * a poll option of a poll that has been answered by the authedUser.  */
+const AnsweredOption = ({
+  optionText,
+  questionVotes,
+  totalVotes,
+  chosenByAuthedUser,
+}) => {
+  const winning = questionVotes / totalVotes >= 0.5;
+  return (
+    <div className="container-column question-option-container">
+      <span className="question-option">{optionText}</span>
+
+      <span
+        className={`question-answer-percentage ${
+          winning ? "winning-vote" : "losing-vote"
+        }`}
+      >
+        Votes: {Math.round((questionVotes / totalVotes) * 100)}%
+      </span>
+    </div>
+  );
+};
+
+export default AnsweredOption;
---
+++ src/components/UserAnswer.js
@@ -0,0 +1,18 @@
+/* UserAnswer is a presentational component that shows
+ * the poll answer provided by the user.  */
+const UserAnswer = ({
+  answeredOptionOne,
+  answeredOptionTwo,
+  optionOneText,
+  optionTwoText,
+}) => {
+  return (
+    <div className="user-answer">
+      {"You voted for... "}
+      {(answeredOptionOne && optionOneText) ||
+        (answeredOptionTwo && optionTwoText)}
+    </div>
+  );
+};
+
+export default UserAnswer;
