b421a77da822ee591ec43be8e683bc16cbe677a3
Nick Anthony <nickanthony45@gmail.com>
2023-02-13 17:12:53 -0500

    Move App.css into styles/App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import "../App.css";
+import "../styles/App.css";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import { useEffect, Fragment } from "react";
