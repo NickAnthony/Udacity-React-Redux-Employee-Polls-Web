eb4c27d83b58bb67bd961de433e658286a8520fb
Nick Anthony <nickanthony45@gmail.com>
2023-02-14 13:16:02 -0500

    Remove some unused imports

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -2,13 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducers from "../reducers";
 import middleware from "../middleware";
 import { users, questions } from "../utils/_DATA";
-import CreateQuestion from "../components/CreateQuestion";
-import CreateUser from "../components/CreateUser";
 import Dashboard from "../components/Dashboard";
-import Leaderboard from "../components/Leaderboard";
-import Login from "../components/Login";
 import Question from "../components/Question";
-import UserProfile from "../components/UserProfile";
 import { Provider } from "react-redux";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
